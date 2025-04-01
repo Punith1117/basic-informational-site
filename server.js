f48393208cf1f91d93a0d11164b360d4aa235351
Punith1117 <punith11172006@gmail.com>
2025-04-01 23:09:13 +0530

    add logic to send response based on url using switch statement

--- server.js
+++ server.js
@@ -1,8 +1,51 @@
 const http = require('node:http')
+const path = require('node:path')
+const fs = require('node:fs')
 
 const server = http.createServer((req, res) => {
-    console.log(req.url)
-    res.end()
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    switch (req.url) {
+        case '/': {
+            console.log('index.html')
+            fs.readFile(path.join(__dirname, 'html', 'index.html'), (err, content) => {
+                if (err)
+                    console.error(err)
+                res.end(content)
+            })
+            break
+        }
+        case '/about': {
+            console.log('about.html')
+            fs.readFile(path.join(__dirname, 'html', 'about.html'), (err, content) => {
+                if (err)
+                    console.error(err)
+                res.end(content)
+            })
+            break
+        }
+        case '/contact': {
+            console.log('contact-me.html')
+            fs.readFile(path.join(__dirname, 'html', 'contact-me.html'), (err, content) => {
+                if (err)
+                    console.error(err)
+                res.end(content)
+            })
+            break
+        }
+        case '/favicon.ico': { 
+            console.log('favicon')
+            res.end()
+            break
+        }
+        default: {
+            console.log('404 not found page')
+            fs.readFile(path.join(__dirname, 'html', '404.html'), (err, content) => {
+                if (err)
+                    console.error(err)
+                res.end(content)
+            })
+        }
+    }
 })
 
 const PORT = 8080
